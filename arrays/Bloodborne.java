3116dd8501c2defe12b82091aabfa56895c6cf83
Montserrat <100958236+benildda@users.noreply.github.com>
2025-12-23 15:23:52 +0100

    new class

--- arrays/Bloodborne.java
+++ arrays/Bloodborne.java
@@ -1,9 +1,41 @@
+import java.util.Scanner;
 
 public class Bloodborne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		Scanner sc = new Scanner(System.in);
 
+		int casos = sc.nextInt();
+
+		for (int i = 0; i < casos; i++) {
+
+			int index = sc.nextInt();
+			int[] arr = new int[index];
+
+			for (int j = 0; j < arr.length; j++) {
+				arr[j] = sc.nextInt();
+
+			}
+
+			boolean respuesta = false;
+
+			for (int k = 0; k < arr.length -1; k++) {
+
+
+					if (arr[k] == arr[k +1]) {
+
+						respuesta = true;
+					}
+				}
+			
+			if (respuesta) {
+				System.out.println("SI");
+			} else {
+				System.out.println("NO");
+			}
+
+		}
+	}
 }
