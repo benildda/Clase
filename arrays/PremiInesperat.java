97f30c3015b50a65ff93209d50ecf5f2ddee0158
Montserrat <100958236+benildda@users.noreply.github.com>
2025-12-27 00:13:59 +0100

    Update PremiInesperat.java

--- arrays/PremiInesperat.java
+++ arrays/PremiInesperat.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 
 public class PremiInesperat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
 
 	}
 
