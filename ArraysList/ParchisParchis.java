76361048df09e50e15a81899dc47d5667ce57ef8
Montserrat <100958236+benildda@users.noreply.github.com>
2025-12-28 00:42:31 +0100

    Update ParchisParchis.java

--- ArraysList/ParchisParchis.java
+++ ArraysList/ParchisParchis.java
@@ -1,10 +1,30 @@
 package ArraysList;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ParchisParchis {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		int casos = sc.nextInt();
+		
+		
+		
+		for (int i=0;i<casos;i++) {
+		ArrayList<Integer> partida = new ArrayList<>();
+
+		for (int h=0;h<partida.size();h++) {
+			
+			partida.add(sc.nextInt());
+			
+			
+			
+		}}
+		
 	}
 
 }
