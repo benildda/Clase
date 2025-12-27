d2b19c86396d468e21a051ef2b342a6f05755fef
Montserrat <100958236+benildda@users.noreply.github.com>
2025-12-27 10:56:07 +0100

    Update PremiInesperat.java

--- arrays/PremiInesperat.java
+++ arrays/PremiInesperat.java
@@ -6,26 +6,34 @@ public class PremiInesperat {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
-		ArrayList<Integer> lista = new ArrayList<>();
-		
+
 		int valor = sc.nextInt();
-		int carlota = sc.nextInt();
-		int joana = sc.nextInt();
+
 		String carlota1 = "CARLOTA";
 		String joana1 = "JOANA";
-		
+
 		while (valor != 0) {
-		    lista.add(valor);
-		    valor = sc.nextInt();
-		    
-		}System.out.println(lista);
-		
-		    
-		    
+			ArrayList<Integer> lista = new ArrayList<>();
+
+			while (valor != 0) {
+				lista.add(valor);
+				valor = sc.nextInt();
+			}
+
+			int carlota = sc.nextInt();
+			int joana = sc.nextInt();
+
+			int joanaResult = lista.get(joana - 1);
+			int carlotaResult = lista.get(carlota - 1);
+
+			if (joanaResult < carlotaResult) {
+				System.out.println(joana1);
+			} else {
+				System.out.println(carlota1);
+			}
+			valor = sc.nextInt();
 		}
-		
 
 	}
 
-
+}
