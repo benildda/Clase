b8fbc72dc1fb2e297a64d1a4c2bcde190243907a
Montserrat <100958236+benildda@users.noreply.github.com>
2025-12-27 01:24:27 +0100

    Update PremiInesperat.java

--- arrays/PremiInesperat.java
+++ arrays/PremiInesperat.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PremiInesperat {
@@ -6,9 +7,25 @@ public class PremiInesperat {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
+		ArrayList<Integer> lista = new ArrayList<>();
 		
+		int valor = sc.nextInt();
+		int carlota = sc.nextInt();
+		int joana = sc.nextInt();
+		String carlota1 = "CARLOTA";
+		String joana1 = "JOANA";
+		
+		while (valor != 0) {
+		    lista.add(valor);
+		    valor = sc.nextInt();
+		    
+		}System.out.println(lista);
+		
+		    
+		    
+		}
 		
 
 	}
 
-}
+
