5a137b110e0d9b4a8f92347fc9024f42291ca290
Montserrat <100958236+benildda@users.noreply.github.com>
2025-12-28 19:17:24 +0100

    Update ParchisParchis.java

--- ArraysList/ParchisParchis.java
+++ ArraysList/ParchisParchis.java
@@ -9,21 +9,45 @@ public class ParchisParchis {
 		// TODO Auto-generated method stub
 
 		Scanner sc = new Scanner(System.in);
-		
-		int casos = sc.nextInt();
-		
-		
-		
-		for (int i=0;i<casos;i++) {
-		ArrayList<Integer> partida = new ArrayList<>();
 
-		for (int h=0;h<partida.size();h++) {
+		int casos = sc.nextInt();
+
+		for (int i = 0; i < casos; i++) {
+
+			ArrayList<Integer> partida = new ArrayList<>();
+
+			for (int h = 0; h < 4; h++) {
+                partida.add(sc.nextInt());
+            }
+
+            int posicion = partida.get(0);      
+            boolean ganado = false;         
+
+           
+            for (int t = 1; t <= 3; t++) {
+                int tiradas = partida.get(t);
+
+                if (!ganado) {
+                	
+                    int sumaDados = posicion + tiradas;
+
+                    if (sumaDados <= 8) {
+                        posicion = sumaDados;
+                    } else {
+                      
+                        posicion = 16 - sumaDados;   
+                    }
+
+                    if (posicion == 8) {
+                        ganado = true;
+                    }
+                }
+            }
+
+            System.out.println(posicion);
 			
-			partida.add(sc.nextInt());
 			
-			
-			
-		}}
+		}
 		
 	}
 
