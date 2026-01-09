3e339364496aa8278c03d71f2278e2a122652ff8
Montserrat <100958236+benildda@users.noreply.github.com>
2026-01-09 20:36:37 +0100

    Update Colarse.java

--- ArraysList/Colarse.java
+++ ArraysList/Colarse.java
@@ -1,10 +1,28 @@
 package ArraysList;
 
+import java.util.Scanner;
+
 public class Colarse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		int casos = sc.nextInt();
+		
+		for (int i = 0; i < casos; i++) {
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		
 	}
 
 }
