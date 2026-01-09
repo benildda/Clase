7a99232c0c6dec7eff816fd1b6752d4a5d863011
Montserrat <100958236+benildda@users.noreply.github.com>
2026-01-09 17:12:00 +0100

    Create Frequencia.java

---
+++ ArraysList/Frequencia.java
@@ -0,0 +1,15 @@
+package ArraysList;
+
+import java.util.Scanner;
+
+public class Frequencia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
