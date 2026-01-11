756317c687383b5970aaed76680caa0b514f74df
Montserrat <100958236+benildda@users.noreply.github.com>
2026-01-11 18:18:47 +0100

    Create productenumeros.java

---
+++ Scanner/productenumeros.java
@@ -0,0 +1,40 @@
+package Scanner;
+
+import java.util.Scanner;
+
+public class productenumeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int casos = sc.nextInt();
+		
+		
+		for(int j=0;j<casos;j++) {
+			
+			int nombres = sc.nextInt();
+			
+			
+			if(nombres <= 0) {
+				System.out.println("ELS NOMBRES NATURALS COMENCEN EN 1");
+			} 
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+	}
+
+}
