697e36d70565646db63af76d3765a6a3da65938e
Montserrat <100958236+benildda@users.noreply.github.com>
2026-01-11 17:31:04 +0100

    Create totsmoltaladreta.java

---
+++ ArraysList/totsmoltaladreta.java
@@ -0,0 +1,40 @@
+package ArraysList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class totsmoltaladreta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		int casos = sc.nextInt();
+		
+		
+		for (int m=0;m<casos;m++) {
+			
+			int longuest = sc.nextInt();
+			
+			ArrayList<Integer> derecha = new ArrayList<>(longuest);
+			
+			for (int y=0;y<longuest;y++) {
+				
+				derecha.add(sc.nextInt());
+				
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+	}
+
+}
