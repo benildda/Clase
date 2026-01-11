8ecd53c5efccc3f23d2642435ee4863052e40982
Montserrat <100958236+benildda@users.noreply.github.com>
2026-01-11 16:30:54 +0100

    Update PanettoneVip.java

--- ArraysList/PanettoneVip.java
+++ ArraysList/PanettoneVip.java
@@ -1,6 +1,7 @@
 package ArraysList;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class PanettoneVip {
@@ -9,48 +10,48 @@ public class PanettoneVip {
 		// TODO Auto-generated method stub
 
 		Scanner sc = new Scanner(System.in);
-		
-		
-		
+
 		int casos = sc.nextInt();
-		
-		for (int i=0;i<casos;i++) {
-			
+
+		for (int i = 0; i < casos; i++) {
+
 			int longitudArray = sc.nextInt();
-			
+
 			ArrayList<String> alumnos = new ArrayList<>(longitudArray);
-			
-			
-			for (int g=0;g<longitudArray;g++) {
-				
+
+			for (int g = 0; g < longitudArray; g++) {
+
 				alumnos.add(sc.next());
-				
+
 			}
-			
+
 			ArrayList<Integer> posicion = new ArrayList<Integer>();
-			
-			for (int p=0;p<alumnos.size();p++) {
-				
+
+			for (int p = 0; p < alumnos.size(); p++) {
+
 				posicion.add(sc.nextInt());
-				
+
 			}
-			
-			
-			
-			
-			
-			
-			System.out.println(alumnos);
+
+			for (int a = 0; a < longitudArray - 1; a++) {
+				for (int b = a + 1; b < longitudArray; b++) {
+
+					if (posicion.get(a) < posicion.get(b)) {
+
+						Collections.swap(posicion, a, b);
+						Collections.swap(alumnos, a, b);
+
+					}
+
+				}
+
+			}
+			for (int k = 0; k < alumnos.size(); k++) {
+				System.out.print(alumnos.get(k));
+				if (k < alumnos.size() - 1)
+					System.out.print(" ");
+			}
 			System.out.println();
-			
-			
-			
-			
-			
-			
-			
-			
 		}
 	}
-
 }
