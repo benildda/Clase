64b0bd99011f3249491c346d0e2a18a1e2f34d2a
Montserrat <100958236+benildda@users.noreply.github.com>
2026-01-25 22:32:51 +0100

    Update comandes.java

--- Inventari/comandes.java
+++ Inventari/comandes.java
@@ -2,6 +2,22 @@ package Inventari;
 
 public class comandes {
 
-	
+	class Productes {
+
+constructor(name, age, alias){
+
+this.name = name;
+thi.age = age;
+this.alias = alias;
+
 
 }
+
+
+
+    }
+
+}
+
+
+
