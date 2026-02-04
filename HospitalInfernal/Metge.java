a87cf9495c8b1274950321ed86d5470c5998223a
Montserrat <100958236+benildda@users.noreply.github.com>
2026-02-04 21:00:43 +0100

    jkkj

--- HospitalInfernal/Metge.java
+++ HospitalInfernal/Metge.java
@@ -1,10 +1,13 @@
 package HospitalInfernal;
 
+import java.util.ArrayList;
+
 public class Metge {
     //atributs del pacient
     String nom;
     int anysTreballats;
     double sou;
+    String departament;
 
 
     public void diguesNom() {
@@ -12,4 +15,37 @@ public class Metge {
         //this és una paraula especial que fa referència a nosaltres mateixos.
         System.out.println(this.nom);
     }
+ 
+    
+    public void argumentarAnysTreballats() {
+    this.anysTreballats++;
+    this.sou = sou *100/2;
+    if(sou%6==0) {
+    	this.sou = sou *100/5;
+    }
+    //fixa't que el constructor no té retorn, ja que el que retorna és la pròpia classe
+    public Metge() {
+        this.nom = "Sense definir";
+        this.anysTreballats = 0;
+        this.sou = 1000.0;
+    }
+    //podem crear múltiples constructors sempre que tinguin paràmetres diferents
+    public Metge(String nom) {
+        //this.nom fa referència a la variable global i nom a la local que hem passat per paràmetre
+        this.nom = nom;
+        this.anysTreballats = 0;
+        this.sou = 1000.0;
+    }
+    
+    public Metge(String nom, int anysTreballats) {
+        //aquest constructor crida al constructor de nom primer, per això el this(nom), amb el que farà tot el que el constructor de nom digui
+        this(nom);
+        //podem fer fors dintre d'un constructor com si fos una funció normal
+        for (int i = 0; i < anysTreballats; i++) {
+            //podem cridar a funcions de la nostra pròpia classe amb el this
+            this.augmentarAnysTreballats();
+        }
+    
+}
+    
 }
